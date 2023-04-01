1038fac152191e029aed7b55b7bfb75e5a7d05fc
martin setaro <lordsesony@gmail.com>
2023-04-01 13:29:54 -0300

    probando configuracion de imagenes

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from './styles.module.scss';
 import Image from 'next/image';
-import pic1 from '../../public/img/pic1.avif'
+import pic1 from '../../public/img/pic1.webp'
 import pic2 from '../../public/img/planetas.webp'
 
 
